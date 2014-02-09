48653109cd823dce0943bcda13aa73cf084c50b7
Jason Viers <bean@beanalby.net>
2014-02-09 16:24:32 -0500

    Initial commit

---
+++ Assets/Editor/PlanetCute2DTools.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlanetCute2DTools : Editor {
+
+    private const float VERTICAL_DIST = .42f;
+
+    [MenuItem("PlanetCute2D/Fix Order %&f")]
+    public static void FixOrder() {
+        // all the ground exists at half intervals of y, so set the order as
+        // double its position
+        foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Ground")) {
+            SpriteRenderer rend = obj.GetComponent<SpriteRenderer>();
+            rend.sortingOrder = Mathf.RoundToInt(obj.transform.position.y / VERTICAL_DIST);
+        }
+    }
+}
---
+++ Assets/Standard Assets/PlanetCute/Scripts/TestPlayer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPlayer : MonoBehaviour {
+    public void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            rigidbody2D.velocity = new Vector3(0, 5, 0);
+        }
+    }
+}
