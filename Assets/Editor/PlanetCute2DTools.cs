454f652d3d6511909f041214465bb9492143f182
Jason Viers <bean@beanalby.net>
2014-02-09 17:10:31 -0500

    Added shadowMaker for 2D

--- Assets/Editor/PlanetCute2DTools.cs
+++ Assets/Editor/PlanetCute2DTools.cs
@@ -16,4 +16,56 @@ public class PlanetCute2DTools : Editor {
             rend.sortingOrder = Mathf.RoundToInt(obj.transform.position.y / VERTICAL_DIST);
         }
     }
+
+    [MenuItem("PlanetCute2D/Remove Shadows %&r")]
+    public static void RemoveShadows() {
+        foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Shadow")) {
+            DestroyImmediate(obj);
+        }
+    }
+
+    [MenuItem("PlanetCute2D/Make Shadows %&m")]
+    public static void MakeShadows() {
+        GameObject obj = GameObject.Find("ShadowMakerHelper");
+        if(obj == null) {
+            Debug.LogError("No ShadowMakerHelper in the scene, please drag in the prefab");
+            return;
+        }
+        ShadowMakerHelper helper = obj.GetComponent<ShadowMakerHelper>();
+
+        RemoveShadows();
+        foreach(GameObject ground in GameObject.FindGameObjectsWithTag("Ground")) {
+            MakeShadow(helper, ground);
+        }
+    }
+
+    private static void MakeShadow(ShadowMakerHelper helper, GameObject ground) {
+        // if there's a block above us, don't bother
+        if(IsGroundPresent(ground, new Vector2(0, 1))) {
+            return;
+        }
+        // if there's a block above to the left, make a west shadow
+        if(IsGroundPresent(ground, new Vector2(-1, 1))) {
+            Debug.Log("Making west shadow for " + ground.name);
+            GameObject obj = Instantiate(helper.shadowWest) as GameObject;
+            obj.transform.parent = ground.transform;
+            obj.transform.position = ground.transform.position;
+        }
+        // if there's a block above to the right, make an east shadow
+        if(IsGroundPresent(ground, new Vector2(1, 1))) {
+            GameObject obj = Instantiate(helper.shadowEast) as GameObject;
+            obj.transform.parent = ground.transform;
+            obj.transform.position = ground.transform.position;
+        }
+
+    }
+
+    private static bool IsGroundPresent(GameObject ground, Vector2 offset) {
+        offset.y -= .5f;
+        offset.y *= VERTICAL_DIST;
+        Vector3 checkPos = ground.transform.position + (Vector3)offset;
+        //Debug.Log(ground.name + " checking at " + checkPos);
+        return Physics2D.OverlapCircle(checkPos, .05f,
+            1 << LayerMask.NameToLayer("Ground")) != null;
+    }
 }
---
+++ Assets/Standard Assets/PlanetCute/Scripts/ShadowMakerHelper.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowMakerHelper : MonoBehaviour {
+    public GameObject shadowEast, shadowWest;
+}
